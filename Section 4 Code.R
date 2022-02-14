adb064055560857354aa7d2c643bb2c3855eb26e
tetracycloide <tetracycloide@gmail.com>
2022-02-14 10:47:39 -0500

    Update Section 4 notes Start Section 6

--- Section 4 Code.R
+++ Section 4 Code.R
@@ -456,6 +456,248 @@ ps <- p_calc(taus)
 # Plot `taus` on the x-axis and `ps` on the y-axis
 plot(taus, ps)
 
+# Code: Definition of results object
+# This code from previous videos defines the results object used for empirical Bayes election forecasting.
+library(tidyverse)
+library(dslabs)
+polls <- polls_us_election_2016 %>%
+  filter(state == "U.S." & enddate >= "2016-10-31" &
+           (grade %in% c("A+", "A", "A-", "B+") | is.na(grade))) %>%
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100)
+one_poll_per_pollster <- polls %>% group_by(pollster) %>%
+  filter(enddate == max(enddate)) %>%
+  ungroup()
+results <- one_poll_per_pollster %>%
+  summarize(avg = mean(spread), se = sd(spread)/sqrt(length(spread))) %>%
+  mutate(start = avg - 1.96*se, end = avg + 1.96*se)
+# Code: Computing the posterior mean, standard error, credible interval and probability
+# Note that to compute an exact 95% credible interval, we would use qnorm(.975) instead of 1.96.
+mu <- 0
+tau <- 0.035
+sigma <- results$se
+Y <- results$avg
+B <- sigma^2 / (sigma^2 + tau^2)
+posterior_mean <- B*mu + (1-B)*Y
+posterior_se <- sqrt(1 / (1/sigma^2 + 1/tau^2))
+posterior_mean
+posterior_se
+# 95% credible interval
+posterior_mean + c(-1.96, 1.96)*posterior_se
+# probability of d > 0
+1 - pnorm(0, posterior_mean, posterior_se)
+# Code: Simulated data with xsubj=d+ejsubj
+J <- 6
+N <- 2000
+d <- .021
+p <- (d+1)/2
+X <- d + rnorm(J, 0, 2*sqrt(p*(1-p)/N))
+# Code: Simulated data with xsubi,j=d+esubi,j
+I <- 5
+J <- 6
+N <- 2000
+d <- .021
+p <- (d+1)/2
+X <- sapply(1:I, function(i){
+  d + rnorm(J, 0, 2*sqrt(p*(1-p)/N))
+})
+# Code: Simulated data with xdubi,j = d + hsubi + esubi,j
+I <- 5
+J <- 6
+N <- 2000
+d <- .021
+p <- (d+1)/2
+h <- rnorm(I, 0, 0.025)    # assume standard error of pollster-to-pollster variability is 0.025
+X <- sapply(1:I, function(i){
+  d + rnorm(J, 0, 2*sqrt(p*(1-p)/N))
+})
+# Code: Calculating probability of d>0 with general bias
+# Note that sigma now includes an estimate of the variability due to general bias of .025.
+mu <- 0
+tau <- 0.035
+sigma <- sqrt(results$se^2 + .025^2)
+Y <- results$avg
+B <- sigma^2 / (sigma^2 + tau^2)
+posterior_mean <- B*mu + (1-B)*Y
+posterior_se <- sqrt(1 / (1/sigma^2 + 1/tau^2))
+1 - pnorm(0, posterior_mean, posterior_se)
+# Code: Top 5 states ranked by electoral votes
+# The results_us_election_2016 object is defined in the dslabs package:
+  library(tidyverse)
+library(dslabs)
+data("polls_us_election_2016")
+head(results_us_election_2016)
+
+results_us_election_2016 %>% arrange(desc(electoral_votes)) %>% top_n(5, electoral_votes)
+# Code: Computing the average and standard deviation for each state
+results <- polls_us_election_2016 %>%
+  filter(state != "U.S." &
+           !grepl("CD", state) &
+           enddate >= "2016-10-31" &
+           (grade %in% c("A+", "A", "A-", "B+") | is.na(grade))) %>%
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100) %>%
+  group_by(state) %>%
+  summarize(avg = mean(spread), sd = sd(spread), n = n()) %>%
+  mutate(state = as.character(state))
+# 10 closest races = battleground states
+results %>% arrange(abs(avg))
+# joining electoral college votes and results
+results <- left_join(results, results_us_election_2016, by="state")
+# states with no polls: note Rhode Island and District of Columbia = Democrat
+results_us_election_2016 %>% filter(!state %in% results$state)
+# assigns sd to states with just one poll as median of other sd values
+results <- results %>%
+  mutate(sd = ifelse(is.na(sd), median(results$sd, na.rm = TRUE), sd))
+# Code: Calculating the posterior mean and posterior standard error
+# Note there is a small error in the video code: B should be defined as sigma^2/(sigma^2 + tau^2).
+mu <- 0
+tau <- 0.02
+results %>% mutate(sigma = sd/sqrt(n),
+                   B = sigma^2/ (sigma^2 + tau^2),
+                   posterior_mean = B*mu + (1-B)*avg,
+                   posterior_se = sqrt( 1 / (1/sigma^2 + 1/tau^2))) %>%
+  arrange(abs(posterior_mean))
+# Code: Monte Carlo simulation of Election Night results (no general bias)
+mu <- 0
+tau <- 0.02
+clinton_EV <- replicate(1000, {
+  results %>% mutate(sigma = sd/sqrt(n),
+                     B = sigma^2/ (sigma^2 + tau^2),
+                     posterior_mean = B*mu + (1-B)*avg,
+                     posterior_se = sqrt( 1 / (1/sigma^2 + 1/tau^2)),
+                     simulated_result = rnorm(length(posterior_mean), posterior_mean, posterior_se),
+                     clinton = ifelse(simulated_result > 0, electoral_votes, 0)) %>%    # award votes if Clinton wins state
+    summarize(clinton = sum(clinton)) %>%    # total votes for Clinton
+    .$clinton + 7    # 7 votes for Rhode Island and DC
+})
+mean(clinton_EV > 269)    # over 269 votes wins election
+# histogram of outcomes
+data.frame(clintonEV) %>%
+  ggplot(aes(clintonEV)) +
+  geom_histogram(binwidth = 1) +
+  geom_vline(xintercept = 269)
+# Code: Monte Carlo simulation including general bias
+mu <- 0
+tau <- 0.02
+bias_sd <- 0.03
+clinton_EV_2 <- replicate(1000, {
+  results %>% mutate(sigma = sqrt(sd^2/(n) + bias_sd^2),    # added bias_sd term
+                     B = sigma^2/ (sigma^2 + tau^2),
+                     posterior_mean = B*mu + (1-B)*avg,
+                     posterior_se = sqrt( 1 / (1/sigma^2 + 1/tau^2)),
+                     simulated_result = rnorm(length(posterior_mean), posterior_mean, posterior_se),
+                     clinton = ifelse(simulated_result > 0, electoral_votes, 0)) %>%    # award votes if Clinton wins state
+    summarize(clinton = sum(clinton)) %>%    # total votes for Clinton
+    .$clinton + 7    # 7 votes for Rhode Island and DC
+})
+mean(clinton_EV_2 > 269)    # over 269 votes wins election
+# Code: Variability across one pollster
+# select all national polls by one pollster
+one_pollster <- polls_us_election_2016 %>%
+  filter(pollster == "Ipsos" & state == "U.S.") %>%
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100)
+
+# the observed standard error is higher than theory predicts
+se <- one_pollster %>%
+  summarize(empirical = sd(spread),
+            theoretical = 2*sqrt(mean(spread)*(1-mean(spread))/min(samplesize)))
+se
+
+# the distribution of the data is not normal
+one_pollster %>% ggplot(aes(spread)) +
+  geom_histogram(binwidth = 0.01, color = "black")
+# Code: Trend across time for several pollsters
+polls_us_election_2016 %>%
+  filter(state == "U.S." & enddate >= "2016-07-01") %>%
+  group_by(pollster) %>%
+  filter(n() >= 10) %>%
+  ungroup() %>%
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100) %>%
+  ggplot(aes(enddate, spread)) +
+  geom_smooth(method = "loess", span = 0.1) +
+  geom_point(aes(color = pollster), show.legend = FALSE, alpha = 0.6)
+# Code: Plotting raw percentages across time
+polls_us_election_2016 %>%
+  filter(state == "U.S." & enddate >= "2016-07-01") %>%
+  select(enddate, pollster, rawpoll_clinton, rawpoll_trump) %>%
+  rename(Clinton = rawpoll_clinton, Trump = rawpoll_trump) %>%
+  gather(candidate, percentage, -enddate, -pollster) %>%
+  mutate(candidate = factor(candidate, levels = c("Trump", "Clinton"))) %>%
+  group_by(pollster) %>%
+  filter(n() >= 10) %>%
+  ungroup() %>%
+  ggplot(aes(enddate, percentage, color = candidate)) +
+  geom_point(show.legend = FALSE, alpha = 0.4) +
+  geom_smooth(method = "loess", span = 0.15) +
+  scale_y_continuous(limits = c(30, 50))
+# Code: Calculating 95% confidence intervals with the t-distribution
+z <- qt(0.975, nrow(one_poll_per_pollster) - 1)
+one_poll_per_pollster %>%
+  summarize(avg = mean(spread), moe = z*sd(spread)/sqrt(length(spread))) %>%
+  mutate(start = avg - moe, end = avg + moe)
+# quantile from t-distribution versus normal distribution
+qt(0.975, 14)    # 14 = nrow(one_poll_per_pollster) - 1
+qnorm(0.975)
+# Association tests
+# Code: Research funding rates example
+# load and inspect research funding rates object
+library(tidyverse)
+library(dslabs)
+data(research_funding_rates)
+research_funding_rates
+# compute totals that were successful or not successful
+totals <- research_funding_rates %>%
+  select(-discipline) %>%
+  summarize_all(funs(sum)) %>%
+  summarize(yes_men = awards_men,
+            no_men = applications_men - awards_men,
+            yes_women = awards_women,
+            no_women = applications_women - awards_women)
+# compare percentage of men/women with awards
+totals %>% summarize(percent_men = yes_men/(yes_men + no_men),
+                     percent_women = yes_women/(yes_women + no_women))
+# Code: Two-by-two table and p-value for the Lady Tasting Tea problem
+tab <- matrix(c(3,1,1,3), 2, 2)
+rownames(tab) <- c("Poured Before", "Poured After")
+colnames(tab) <- c("Guessed Before", "Guessed After")
+tab
+# p-value calculation with Fisher's Exact Test
+fisher.test(tab, alternative = "greater")
+# Code: Chi-squared test
+# compute overall funding rate
+funding_rate <- totals %>%
+  summarize(percent_total = (yes_men + yes_women) / (yes_men + no_men + yes_women + no_women)) %>%
+  .$percent_total
+funding_rate
+# construct two-by-two table for observed data
+two_by_two <- tibble(awarded = c("no", "yes"),
+                     men = c(totals$no_men, totals$yes_men),
+                     women = c(totals$no_women, totals$yes_women))
+two_by_two
+# compute null hypothesis two-by-two table
+tibble(awarded = c("no", "yes"),
+       men = (totals$no_men + totals$yes_men) * c(1-funding_rate, funding_rate),
+       women = (totals$no_women + totals$yes_women) * c(1-funding_rate, funding_rate))
+# chi-squared test
+chisq_test <- two_by_two %>%
+  select(-awarded) %>%
+  nbsp;   chisq.test()
+chisq_test$p.value
+# Code: Odds ratio
+# odds of getting funding for men
+odds_men <- (two_by_two$men[2] / sum(two_by_two$men)) /
+  (two_by_two$men[1] / sum(two_by_two$men))
+# odds of getting funding for women
+odds_women <- (two_by_two$women[2] / sum(two_by_two$women)) /
+  (two_by_two$women[1] / sum(two_by_two$women))
+# odds ratio - how many times larger odds are for men than women
+odds_men/odds_women
+# Code: p-value and odds ratio responses to increasing sample size
+# multiplying all observations by 10 decreases p-value without changing odds ratio
+two_by_two %>%
+  select(-awarded) %>%
+  mutate(men = men*10, women = women*10) %>%
+  chisq.test()
+
 
 
 
